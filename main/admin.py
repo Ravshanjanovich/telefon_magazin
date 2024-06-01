fc4821c88a9b5d8d07d177ade4c0135672234c97
Ravshanjanovich <aslbekqurbonboyev12@gmail.com>
2024-06-01 09:36:30 +0500

    [admin py full]

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
+from .models import Contact, Testemonial, History, About , Index 
 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('title','name','description',)
+    list_display_links = ('title','name','description',)
+
+@admin.register(Testemonial)
+class TestemonialAdmin(admin.ModelAdmin):
+    list_display = ('name','description','image',)
+    list_display_links = ('name','description','image',)
+
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('title','description',)
+    list_display_links = ('title','description',)
+
+@admin.register(About)
+class AboutAdmin(amdin.ModelAdmin):
+    list_display = ('title','sub_title','image',)
+    list_display_links = ('title','sub_title','image',)
+
+@admin.register(Index)
+class IndexAdmin(admin.ModelAdmin):
+    list_display = ('title','sub_title','image',)
+    list_display_links = ('title','sub_title','image',)
